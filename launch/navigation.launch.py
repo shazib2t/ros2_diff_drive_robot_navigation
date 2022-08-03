bfa6b8c798e75d048d7f228be97071e3c5052fa8
shazib2t <shazib2t@gmail.com>
2022-08-03 17:14:04 -0400

    ros2 differential drive robot nav2 packages

---
+++ launch/navigation.launch.py
@@ -0,0 +1,68 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+MAP_NAME='playground' #change to the name of your own map here
+
+def generate_launch_description():
+    depth_sensor = os.getenv('ROS2_DIFF_DRIVE_ROBOT_DEPTH_SENSOR', '')
+
+    nav2_launch_path = PathJoinSubstitution(
+        [FindPackageShare('nav2_bringup'), 'launch', 'bringup_launch.py']
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('ros2_diff_drive_robot_navigation'), 'rviz', 'ros2_diff_drive_robot_navigation.rviz']
+    )
+
+    default_map_path = PathJoinSubstitution(
+        [FindPackageShare('ros2_diff_drive_robot_navigation'), 'maps', f'{MAP_NAME}.yaml']
+    )
+
+    nav2_config_path = PathJoinSubstitution(
+        [FindPackageShare('ros2_diff_drive_robot_navigation'), 'config', 'navigation.yaml']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='sim', 
+            default_value='false',
+            description='Enable use_sime_time to true'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='false',
+            description='Run rviz'
+        ),
+
+       DeclareLaunchArgument(
+            name='map', 
+            default_value=default_map_path,
+            description='Navigation map path'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_path),
+            launch_arguments={
+                'map': LaunchConfiguration("map"),
+                'use_sim_time': LaunchConfiguration("sim"),
+                'params_file': nav2_config_path
+            }.items()
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{'use_sim_time': LaunchConfiguration("sim")}]
+        )
+    ])
